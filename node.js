10a1b45af8b216d6a75301efd013b37e1332a205
ItsMarvoKim <marvin.kithinji@student.moringaschool.com>
2022-03-09 07:11:09 +0300

    creation of data using JS

--- node.js
+++ node.js
@@ -4,4 +4,13 @@ function handleSubmit(event){
     event.preventDefault()
     const dateOfBirth= document.querySelector("#DOB").value;
     console.log(dateOfBirth)
+    const ddmmyy=dateOfBirth.split("-");
+    console.log(ddmmyy)
+    const year=ddmmyy[0];
+    const month=ddmmyy[1];
+    const day=ddmmyy[2];
+    
+
+
+
 }
